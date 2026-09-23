25f70f004e3772bf784509c58c4c95af915b9b15
larafod <laramfodino@gmail.com>
2026-09-22 21:47:08 -0300

    sacar f main

--- principal.py
+++ principal.py
@@ -6,14 +6,7 @@ Responsabilidad: Punto de entrada del programa. Inicializa los datos y coordina 
 from datos import obtener_registros
 from menu import ejecutar_menu
 
-
-def main():
-    """
-    Función principal que inicia el sistema de gestión y análisis de reseñas.
-    """
-    matriz_registros = obtener_registros()
-    ejecutar_menu(matriz_registros)
+matriz_registros = obtener_registros()
+ejecutar_menu(matriz_registros)
 
 
-if __name__ == "__main__":
-    main()
